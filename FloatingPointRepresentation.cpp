34c894239165d00cba51e83810e41fd1fdb0c82f
Marouan Doulahiane <mdoulahi@e2r8p6.1337.ma>
2024-01-15 22:06:20 +0100

    by cypher

--- FloatingPointRepresentation.cpp
+++ FloatingPointRepresentation.cpp
@@ -6,7 +6,7 @@ bool    isValidNumber(string nb)
 {
 	size_t  i = 0;
 
-	if (nb[i] == '+' || nb[i] == '-') 
+	if ((nb[i] == '+' || nb[i] == '-' )&& nb.size() > 1)
 		i++;
 	while (isdigit(nb[i]))
 		i++;
@@ -40,7 +40,6 @@ string  getInputNumber(string msg)
 		cerr << "Error: Bad Input" << endl;
 		exit(1);
 	}
-	cerr << "Error: Good Input" << endl;
 	return input;
 }
 
@@ -87,6 +86,87 @@ void    handleInt(string nb)
 	cout << res << " -> [" << bits << "]" << endl; 
 }
 
+string	getBits(string nb)
+{
+	int res;
+	string bits;
+
+	try {
+		res = stoi(nb);
+	}
+	catch (...) {
+		cerr << "stoi() failed!" << std::endl;
+		exit(1);
+	}
+	int temp = res;
+	while (temp)
+	{
+		bits += temp % 2? '1': '0';
+		temp /= 2;
+	}
+	reverse(bits.begin(), bits.end());
+	return bits;
+}
+
+string	getBits(int res)
+{
+	string bits;
+
+	int temp = res;
+	while (temp)
+	{
+		bits += temp % 2? '1': '0';
+		temp /= 2;
+	}
+	reverse(bits.begin(), bits.end());
+	return bits;
+}
+
+int	stringToInt(string nb)
+{
+	int res;
+
+	try {
+		res = stoi(nb);
+	}
+	catch (...) {
+		cerr << "stoi() failed!" << std::endl;
+		exit(1);
+	}
+	return (res);
+}
+
+void	handleFloat(string integerPart, string floatPart)
+{
+	string	results;
+
+	string	intBits = getBits(integerPart);
+	int		exponentBias = intBits.size() - 1 + 127;
+	string	exponentBits = getBits(exponentBias);
+	results += integerPart[0] == '-'? "1": "0";
+
+	for (int i = 0; i < 8 - exponentBits.size(); i++)
+		results += "0";
+	results += exponentBits;
+	int mantissaPos = 1;
+	if (integerPart[0] == '+' || integerPart[0] == '-')
+		mantissaPos++;
+	cerr << "Test :" << intBits.substr(mantissaPos) << endl;
+	results += intBits.substr(mantissaPos);
+	int i = results.size();
+	int	floatInt = stringToInt(floatPart);
+	int	divider = 10 * floatPart.size();
+	for (; i < 32; i++)
+	{
+		floatInt *= 2;
+		results += floatInt / divider? "1": "0";
+
+		floatInt = floatInt % divider;
+	}
+	cout << "This a float number it's represent in memory like this:" << endl;
+	cout << "myFl(): " << " -> [" << results << "]" << endl; 
+}
+
 int main(void)
 {
 	string  input = getInputNumber("Enter The Number: ");
@@ -111,6 +191,15 @@ int main(void)
 	if (containZeros(fractionalPart))
 		handleInt(integerPart);
 	else
-		handleFloat()
+	{
+		handleFloat(integerPart, fractionalPart);
 
+		float floatValue = stof(input);
+		unsigned int bits = *reinterpret_cast<unsigned int*>(&floatValue);
+		bitset<32> bitRepresentation(bits);
+		cout << "stof():  -> [" << bitRepresentation << "]" << endl;
+
+		
+	}
+	return (0);
 }
